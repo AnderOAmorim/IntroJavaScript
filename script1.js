1e4008bc9fb203feb6ea27f50d07ddec396ae811
AnderOAmorim <andyy.amorim@gmail.com>
2025-03-27 22:04:14 -0300

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,2 +1,16 @@
 console.log("ola mundo");
+//Funcao sem usar o return
+let somar =  function(valor1, valor2){
+    let resultado = valor1 + valor2;
+   console.log(resultado);
+}
+//Execucao da funcao 3 chamadas
+somar(18,25);
+somar(72,37);
+somar(99,1);
 
+let subtrair = function(valor1,valor2){
+    return valor1-valor2;
+}
+let resultado = subtrair(28,12);
+console.log(resultado);
